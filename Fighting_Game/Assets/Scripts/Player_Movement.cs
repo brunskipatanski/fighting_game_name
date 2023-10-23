4be0bf00ecb1af70f7874bf94fc85f44e5076330
glasses <46400975+brunskipatanski@users.noreply.github.com>
2023-10-23 10:37:48 +0300

    major movement
    major movement thig :) didnt add the dtails yeat but in a sec.

--- Fighting_Game/Assets/Scripts/Player_Movement.cs
+++ Fighting_Game/Assets/Scripts/Player_Movement.cs
@@ -5,40 +5,83 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     //speed and jumpforce are controlled by the sidebar >>>>>
+    /* ill add the details later :D */
     public float MoveSpeed;
     public float JumpForce;
+    public float DoubleJumpForce;
+    public float AirDash;
+    public bool MovingLeft;
+    public bool MovingRight;
+    public float MoveForce;
+    public bool DoubleJumped = false;
+    public bool AirDashed = false;
     private Rigidbody2D rb;
     private bool Grounded = false;
+    public bool AllowMove = true;
+    public bool IsMoving = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // used for the jump. since we arent using transform for jumping
     }
 
-    // how basic dummy. mass = 10, gravity = 4, jumpforce = 200, speed like 6  or so
+    // how basic dummy. mass = 10, gravity = 4, jumpforce = 175, speed like 6  or so we can change these
     // fixedupdate makes it so fps doesnt fuck up the game. prblem is is that it missed some frames on jumps
     //so until we find a good way to fix this we will keep it as update
+    //collision detection should be continuous
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (AllowMove)//check for movement
         {
-            transform.position = transform.position + (Vector3.right * MoveSpeed) * Time.deltaTime;
-            
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = transform.position + (Vector3.left * MoveSpeed) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = transform.position + (Vector3.right * MoveSpeed) * Time.deltaTime;
+                MovingRight = true;
+                MovingLeft = false;
+                IsMoving = true;
+                Debug.Log("Player Moving right");
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = transform.position + (Vector3.left * MoveSpeed) * Time.deltaTime;
+                MovingLeft = true;
+                MovingRight = false;
+                IsMoving = true;
+                Debug.Log("Player Moving left");
+            }
+            else
+            {
+                IsMoving = false;
+                MovingLeft = false;
+                MovingRight = false;
+            }
         }
         if (Input.GetKeyDown(KeyCode.W) && Grounded == true)
         {
             //transform.position.AddForce(Vector3.up * JumpForce, ForceMode2D.Impulse);
             rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            AllowMove = false; //main line for making it so you cant move in-air
+            if (MovingLeft == true)
+            {
+                rb.AddForce(Vector2.left * MoveForce, ForceMode2D.Impulse);
+            }
+
+            else if (MovingRight == true)
+            {
+                rb.AddForce(Vector2.right * MoveForce, ForceMode2D.Impulse);
+            }
         }
     }
-    void onCollisionEnter2D(Collision2D collision)
+
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             Grounded = true;
+        }
+        if (Grounded == true)
+        {
+            AllowMove = true;
         }
     }
     void OnCollisionExit2D(Collision2D collision)
